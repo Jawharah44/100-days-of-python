cf910d0c2948b5c9aada74f17cdb095954168dfb
Jawharah44 <joharaalamry@gmail.com>
2026-01-21 13:51:14 +0300

    Add Day 19

---
+++ day19/event_listen.py
@@ -0,0 +1,32 @@
+from turtle import Turtle,Screen
+
+tom =Turtle()
+screen = Screen()
+def move_forward():
+    tom.forward(10)
+    
+def move_backward():
+    tom.backward(10)
+    
+def turn_left():
+    new_heading = tom.heading() + 10
+    tom.setheading(new_heading)
+    
+def turn_right():
+    new_heading = tom.heading() - 10
+    tom.setheading(new_heading)
+
+def clear():
+    tom.clear()
+    tom.penup()
+    tom.home()
+    tom.pendown()
+    
+screen.listen()
+screen.onkey(move_forward,"w")
+screen.onkey(move_backward, "s")
+screen.onkey(turn_left,"a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear, "c")
+
+screen.exitonclick()
