1314ea695ad2ce94b829de8df3b59d043d701d8f
Jawharah44 <joharaalamry@gmail.com>
2026-02-23 09:50:12 +0300

    Add Day 25

---
+++ day25/main.py
@@ -0,0 +1,17 @@
+# with open("day25/weather_data.csv") as weather_info:
+#     data = weather_info.readlines()
+
+# import csv
+
+# with open("day25/weather_data.csv") as weather_info:
+#     data = csv.reader(weather_info)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#         print(row)
+#     print(temperatures)
+
+import pandas as pd
+data = pd.read_csv("day25/weather_data.csv")
+print(data["temp"])
